228dcc304922f374c5298f629a96dc4f7a7271e5
samuel freeman <samuelmanueljnr@gmail.com>
2024-02-11 13:08:23 +0000

    Remove redis error

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,6 @@
 const Redis = require('ioredis');
-const logger = require('./logger');
 const redis = new Redis(
   'rediss://red-cn41k0f109ks73eshuig:Ih2Q9f0B5xJl5H6zRZ4k486V0BiPupVs@oregon-redis.render.com:6379',
 );
-
-redis.on('error', (err) => {
-  logger.error('could not  establish a connection with Redis' + err);
-});
 
 module.exports = redis;
