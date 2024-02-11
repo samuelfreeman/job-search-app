905e6fe678bf3f00b23ecd38dd7919895488dafe
samuel freeman <samuelmanueljnr@gmail.com>
2024-02-11 02:08:43 +0000

    little fix for the redis always throwing message

--- utils/redis.js
+++ utils/redis.js
@@ -1,14 +1,11 @@
 const Redis = require('ioredis');
+const logger = require('./logger');
 const redis = new Redis(
   'rediss://red-cn41k0f109ks73eshuig:Ih2Q9f0B5xJl5H6zRZ4k486V0BiPupVs@oregon-redis.render.com:6379',
 );
 
 redis.on('error', (err) => {
-  console.log;
-  'could not  establish a connection with Redis' + err;
+  logger.error('could not  establish a connection with Redis' + err);
 });
 
-redis.on('connect', (err) => {
-  console.log("connected to redis succesfully");
-});
 module.exports = redis;
